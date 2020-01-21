37459c2268f95de9670beff2c1f7a41336ec6b8b
student <student@BRAVO04.ms.polsl.local>
2020-01-21 11:38:27 +0100

    Ignore lists in dataclass

--- app/src/main/java/com/example/iceandfireapi/data/network/response/IceAndFireResponse.kt
+++ app/src/main/java/com/example/iceandfireapi/data/network/response/IceAndFireResponse.kt
@@ -2,15 +2,19 @@ package com.example.iceandfireapi.data.network.response
 
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "characters")
 data class IceAndFireResponse(
     @SerializedName("aliases")
+    @Ignore
     val aliases: List<String>,
     @SerializedName("allegiances")
+    @Ignore
     val allegiances: List<String>,
     @SerializedName("books")
+    @Ignore
     val books: List<String>,
     @SerializedName("born")
     val born: String,
@@ -27,14 +31,18 @@ data class IceAndFireResponse(
     @SerializedName("name")
     val name: String,
     @SerializedName("playedBy")
+    @Ignore
     val playedBy: List<String>,
     @SerializedName("povBooks")
+    @Ignore
     val povBooks: List<String>,
     @SerializedName("spouse")
     val spouse: String,
     @SerializedName("titles")
+    @Ignore
     val titles: List<String>,
     @SerializedName("tvSeries")
+    @Ignore
     val tvSeries: List<String>,
     @SerializedName("url")
     val url: String
